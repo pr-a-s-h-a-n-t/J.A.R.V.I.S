ec24a079cacee03388aa8dab1d250b78ca7e9198
Prashant Mishra <prashantmishra111@outlook.com>
2023-02-15 14:46:30 +0530

    1st commit!!

---
+++ jarvis/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+ 
+
+function App() {
+  return (
+    <div className=" bg-red-600 flex justify-center ">
+       Hello Coder!!
+    </div>
+  );
+}
+
+export default App;
